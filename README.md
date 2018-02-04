67893907b1c4e5ad64e33413193602f7de721822
Ian Walter <public@iankwalter.com>
2018-02-04 11:57:30 -0500

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # execa-git
+> A wrapper around [execa]() to help run git commands
+
+[![Npm page][npm-image]][npm-url]
+
+## Installation
+
+```fish
+npm install --save execa-git
+```
+
+&nbsp;
+
+ISC &copy; [Ian Walter](https://iankwalter.com)
+
+[npm-image]: https://img.shields.io/npm/v/execa-git.svg
+[npm-url]: https://www.npmjs.com/package/execa-git
--- tests/execa-git.test.js
+++ tests/execa-git.test.js
@@ -1,23 +1,53 @@
-const { Repository, git } = require('../lib')
+const { git, Repository } = require('../lib')
+const { join } = require('path')
+const { existsSync, mkdirSync } = require('fs')
+const del = require('del')
+
+const cwd = '/tmp/execa-git-test'
+const gitPath = join(cwd, '.git')
+
+beforeEach(() => mkdirSync(cwd))
+
+afterEach(async () => del(cwd, { force: true }))
 
 describe('git()', () => {
-  test('', async () => {
-    
+  test('can successfully run git commands', async () => {
+    try {
+      await git('init', { cwd })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      expect(existsSync(gitPath)).toBe(true)
+    }
   })
 })
 
 describe('Repository', () => {
-  const repo = new Repository()
+  const repo = new Repository(cwd)
 
   describe('.do()', () => {
-    test('', async () => {
-      await repo.do()
+    test('can run git commands without specifying cwd', async () => {
+      try {
+        await repo.do('init')
+      } catch (error) {
+        console.error(error)
+      } finally {
+        expect(existsSync(gitPath)).toBe(true)
+      }
     })
   })
-  
+
   describe('.currentBranch()', () => {
-    test('', async () => {
-      const branch = await repo.currentBranch()
+    test('returns the correct current branch', async () => {
+      let branch
+      try {
+        await repo.do('init')
+        branch = await repo.currentBranch()
+      } catch (error) {
+        console.error(error)
+      } finally {
+        expect(branch).toEqual('master')
+      }
     })
   })
 })
